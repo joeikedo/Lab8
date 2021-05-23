8eb0bb45a8fc1a6c45c864ad549bde2e9c8fbffa
joeikedo <54558160+joeikedo@users.noreply.github.com>
2021-05-22 22:26:01 -0700

    All tests working

--- __tests__/lab8.test.js
+++ __tests__/lab8.test.js
@@ -7,8 +7,6 @@ describe('Basic user flow for SPA ', () => {
        
   });
 
-  /*//bruh get rid of this later-----------------------------------------------------------------------------------
-
   // test 1 is given
   it('Test1: Initial Home Page - Check for 10 Journal Entries', async () => {
     const numEntries = await page.$$eval('journal-entry', (entries) => {
@@ -62,8 +60,6 @@ describe('Basic user flow for SPA ', () => {
 
   });
 
-  */ //bruh get rid of this later-----------------------------------------------------------------------------------
-
   it('Test5: On first Entry page - checking <entry-page> contents', async () => {
     /*
      implement test5: Clicking on the first journal entry should contain the following contents: 
@@ -77,25 +73,34 @@ describe('Basic user flow for SPA ', () => {
           }
         }
       */
-    
 
-    /*
+
     await page.$eval( 'journal-entry', form => form.click() ); //Click on first entry
 
+
     const [elementHandle] = await page.$x('/html/body/main/journal-entry[1]');
-    const propertyHandle = await elementHandle.getProperty('innerText');
+    const propertyHandle = await elementHandle.getProperty('entry'); //I guess this is like doing .entry kinda? 
     const propertyValue = await propertyHandle.jsonValue();
 
-    expect(propertyValue).toMatch(/kill me/);
-    */
 
-    //I'm gonna try and come back to this one
+    var obj = new Object();
+    obj.title = "You like jazz?";
+    obj.date  = '4/25/2021';
+    obj.content = "According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway because bees don't care what humans think is impossible.";
+
+    var innerObj = new Object();
+    innerObj.src = 'https://i1.wp.com/www.thepopcornmuncher.com/wp-content/uploads/2016/11/bee-movie.jpg?resize=800%2C455';
+    innerObj.alt = 'bee with sunglasses';
+
+    obj.image = innerObj;
     
-    //!!!!!!!!!TODO!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    expect(propertyValue).toEqual(obj);
+
+    
+
+    
 
   }, 10000);
-
-  /*//bruh get rid of this later -----------------------------------------------------------------------------------
 
   it('Test6: On first Entry page - checking <body> element classes', async () => {
     // implement test6: Clicking on the first journal entry should update the class attribute of <body> to ‘single-entry’
@@ -197,7 +202,7 @@ describe('Basic user flow for SPA ', () => {
 
   });
 
-  */ //bruh get rid of this later -----------------------------------------------------------------------------------
+  
 
 
   // define and implement test13: On the home page the <body> element should not have any class attribute 
@@ -214,20 +219,135 @@ describe('Basic user flow for SPA ', () => {
 
 
   // define and implement test14: Verify the url is correct when clicking on the second entry
+  it('Test14: Verify the url is correct when clicking on the second entry', async () => {
+   
+
+    await page.$$eval( 'journal-entry', form => form[1].click() ); //select the array element at index 1 for the second entry
+
+
+    let placeholder = '/#entry2';
+    expect(page.url()).toMatch(placeholder); 
+
+
+
+  });
 
 
   // define and implement test15: Verify the title is current when clicking on the second entry
+  it('Test15: Verify the title is current when clicking on the second entry', async () => {
+   
 
+    await page.$$eval( 'journal-entry', form => form[1].click() ); //select the array element at index 1 for the second entry
+
+
+    let element = await page.$('h1')
+    let value = await page.evaluate(el => el.textContent, element)
+    
+    expect(value).toMatch(/Entry 2/);
+
+  });
 
   // define and implement test16: Verify the entry page contents is correct when clicking on the second entry
+  it('Test16: Verify the entry page contents is correct when clicking on the second entry', async () => {
+
+    await page.$$eval( 'journal-entry', form => form[1].click() ); //select the array element at index 1 for the second entry
+
+
+    const [elementHandle] = await page.$x('/html/body/main/journal-entry[2]');
+    const propertyHandle = await elementHandle.getProperty('entry'); //I guess this is like doing .entry kinda? 
+    const propertyValue = await propertyHandle.jsonValue();
+
+
+    
+    var obj = new Object();
+    obj.title = "Run, Forrest! Run!";
+    obj.date  = '4/26/2021';
+    obj.content = "Mama always said life was like a box of chocolates. You never know what you're gonna get.";
+
+    var innerObj = new Object();
+    innerObj.src = 'https://s.abcnews.com/images/Entertainment/HT_forrest_gump_ml_140219_4x3_992.jpg';
+    innerObj.alt = 'forrest running';
+
+    obj.image = innerObj;
+    
+    expect(propertyValue).toEqual(obj);
+
+  }, 10000);
+
 
 
   // create your own test 17
+  // define and implement test17: Verify the url is correct when clicking on the third entry
+  it('Test17: Verify the url is correct when clicking on the third entry', async () => {
+   
+
+    await page.$$eval( 'journal-entry', form => form[2].click() ); //select the array element at index 2 for the third entry
+
+
+    let placeholder = '/#entry3';
+    expect(page.url()).toMatch(placeholder); 
+
+
+
+  });
+
 
   // create your own test 18
+  // define and implement test15: Verify the title is current when clicking on the third entry
+  it('Test18: Verify the title is current when clicking on the second entry', async () => {
+   
+
+    await page.$$eval( 'journal-entry', form => form[2].click() ); //select the array element at index 1 for the second entry
+
+
+    let element = await page.$('h1')
+    let value = await page.evaluate(el => el.textContent, element)
+    
+    expect(value).toMatch(/Entry 3/);
+
+  });
+
 
   // create your own test 19
+  it('Test19: Verify the entry page contents is correct when clicking on the third entry', async () => {
+
+    await page.$$eval( 'journal-entry', form => form[2].click() ); //select the array element at index 1 for the second entry
+
+
+    const [elementHandle] = await page.$x('/html/body/main/journal-entry[3]');
+    const propertyHandle = await elementHandle.getProperty('entry'); //I guess this is like doing .entry kinda? 
+    const propertyValue = await propertyHandle.jsonValue();
+
+
+    
+    var obj = new Object();
+    obj.title = "Ogres are like onions";
+    obj.date  = '4/27/2021';
+    obj.content = "Onions have layers. Ogres have layers. Onions have layers. You get it? We both have layers.";
+
+    var innerObj = new Object();
+    innerObj.src = 'https://advancelocal-adapter-image-uploads.s3.amazonaws.com/image.syracuse.com/home/syr-media/width2048/img/entertainment_impact/photo/shrek-donkeyjpg-daa31aa2b5bedfaa.jpg';
+    innerObj.alt = 'shrek and donkey looking confused';
+
+    obj.image = innerObj;
+    
+    expect(propertyValue).toEqual(obj);
+
+  }, 10000);
 
   // create your own test 20
+  // define and implement test20: Verify the url is correct when clicking on the 10th entry
+  it('Test20: Verify the url is correct when clicking on the tenth entry', async () => {
+   
+
+    await page.$$eval( 'journal-entry', form => form[9].click() ); //select the array element at index 2 for the third entry
+
+
+    let placeholder = '/#entry10';
+    expect(page.url()).toMatch(placeholder); 
+
+
+
+  });
   
 });
